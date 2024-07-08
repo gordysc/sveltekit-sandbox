97d2afc1341edf6f096e7e33cc87c2a5e671d4a8
Luke Gordon <gordysc@gmail.com>
2024-07-08 16:45:44 -0500

    chore: add dark mode toggler

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,18 @@
 <script>
   import "../app.css";
+  import { ModeWatcher } from "mode-watcher";
+  import LightSwitch from "@/components/light-switch.svelte";
 </script>
 
-<slot></slot>
+<ModeWatcher />
+
+<div class="container mx-auto">
+  <div class="flex justify-between pt-5">
+    <div class="text-3xl font-semibold">SvelteKit Sandbox</div>
+    <LightSwitch />
+  </div>
+
+  <div class="mt-5">
+    <slot />
+  </div>
+</div>
