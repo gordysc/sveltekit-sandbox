58f45b58124f0355d075594301e7f60abd640b99
Luke Gordon <gordysc@gmail.com>
2024-07-08 16:49:21 -0500

    chore: add resource links

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<article>
+  <div class="text-2xl font-semibold">Resources</div>
+  <ul class="space-y-4 pt-4 list-disc ps-5">
+    <li>
+      SvelteKit: <a class="hover:underline hover:text-orange-500" href="https://kit.svelte.dev/docs/introduction">https://kit.svelte.dev/docs/introduction</a>
+    </li>
+    <li>
+      shadcn-svelte: <a class="hover:underline hover:text-orange-500" href="https://www.shadcn-svelte.com/">https://www.shadcn-svelte.com/</a>
+    </li>
+  </ul>
+</article>
